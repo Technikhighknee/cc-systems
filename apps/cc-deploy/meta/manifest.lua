2a6866d0e166a1d6fbd06e83ccb0ed9bf00e2776
Jason Posch <30759825+Technikhighknee@users.noreply.github.com>
2025-05-28 12:42:24 +0200

    feat(cc-deploy): consolidate installer files

--- apps/cc-deploy/meta/manifest.lua
+++ apps/cc-deploy/meta/manifest.lua
@@ -2,16 +2,15 @@
 return {
   id = "cc-deploy",
   type = "app",
-  version = "0.2.1",
+  version = "0.2.2",
   description = "Installer for cc-systems",
   entry = "apps/cc-deploy/init.lua",
   files = {
     "apps/cc-deploy/init.lua",
     "apps/cc-deploy/meta/manifest.lua",
-    "apps/cc-deploy/init.lua",
     "apps/cc-deploy/install.lua",
     "apps/cc-deploy/registry.lua",
     "apps/cc-deploy/recursive.lua",
-    "apps/cc-utils/require.lua"
+    "modules/cc-utils/require.lua"
   }
 }
